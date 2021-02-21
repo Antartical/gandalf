771cadc869f089b76c9302c58c795b99a8cc0c4e
alvarogf97 <ankora45@gmail.com>
2021-02-21 12:30:26 +0100

    add tests cases

--- middlewares/auth_test.go
+++ middlewares/auth_test.go
@@ -1,1 +1,27 @@
 package middlewares
+
+import "testing"
+
+func TestAuthBearerMiddleware(t *testing.T) {
+	//assert := require.New(t)
+
+	t.Run("Test HasScopes successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test HasScopes wrong token header", func(t *testing.T) {
+
+	})
+
+	t.Run("Test HasScopes unauthorized user", func(t *testing.T) {
+
+	})
+
+	t.Run("Test GetAuthorizedUser successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test GetAuthorizedUser panics", func(t *testing.T) {
+
+	})
+}
--- services/auth_test.go
+++ services/auth_test.go
@@ -1,1 +1,70 @@
 package services
+
+import (
+	"testing"
+)
+
+func TestAuthService(t *testing.T) {
+	//assert := require.New(t)
+
+	t.Run("Test constructor", func(t *testing.T) {
+
+	})
+
+	t.Run("Test getClaims successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test getClaims error", func(t *testing.T) {
+
+	})
+
+	t.Run("Test signToken successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test signToken panics", func(t *testing.T) {
+
+	})
+
+	t.Run("Test Authenticate successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test Authenticate error", func(t *testing.T) {
+
+	})
+
+	t.Run("Test Generate tokens", func(t *testing.T) {
+
+	})
+
+	t.Run("Test GetAuthorizedUser successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test GetAuthorizedUser getClaims error", func(t *testing.T) {
+
+	})
+
+	t.Run("Test GetAuthorizedUser no scopes error", func(t *testing.T) {
+
+	})
+
+	t.Run("Test GetAuthorizedUser no user error", func(t *testing.T) {
+
+	})
+
+	t.Run("Test RefreshToken successfully", func(t *testing.T) {
+
+	})
+
+	t.Run("Test RefreshToken unrecognized token", func(t *testing.T) {
+
+	})
+
+	t.Run("Test RefreshToken unrelates tokens", func(t *testing.T) {
+
+	})
+
+}
